7ab58df1aa44e39a25d9b1425e3aa69eb759d736
christmknd <christmokonda@gmail.com>
2023-05-17 14:06:24 +0200

    Setup tests

--- front/src/App.test.js
+++ front/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {io } from "socket.io-client"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+test('send message via socket.io', () => {
+  const socket= io("http://localhost:9000")
+        const {container} = render( <form><input data-testid="finalInput"/></form> );
+        socket.emit("send:message", 'my message');
+        socket.on("send:message", res => {
+            expect(res).toEqual('my message')
+        })
 });
